735da98d51dc13f35d9589c329f2d491399a51ea
Rajiv S <rajiv620019@gmail.com>
2023-09-11 12:19:06 +0530

    Testing

---
+++ test.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+Contract Test {
+    function test() public {
+        uint256 a = 1;
+        uint256 b = 2;
+        uint256 c = a + b;
+    }
+}
